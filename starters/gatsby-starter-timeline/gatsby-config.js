f4bf191ebab36e68da258bc5517d496e55fe338e
Owen Young <theowenyoung@gmail.com>
2020-10-18 04:51:48 +0800

    chore: publish to surge

--- starters/gatsby-starter-timeline/gatsby-config.js
+++ starters/gatsby-starter-timeline/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Shadowed Site Title`,
-    siteUrl: `https://www.example.com`,
+    siteUrl: `https://gatsby-starter-timeline.surge.sh`,
   },
   plugins: [
     // with gatsby-plugin-theme-ui, the last theme in the config
@@ -15,6 +15,15 @@ module.exports = {
     },
     {
       resolve: `gatsby-theme-timeline-twitter`,
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        policy: [{ userAgent: "*", disallow: ["/"] }],
+      },
     },
   ],
 }
