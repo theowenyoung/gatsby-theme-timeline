1b695021bdd14c7c948379a7667427de7f553e7e
Owen Young <theowenyoung@gmail.com>
2020-11-28 07:23:08 +0800

    fix: transformJson false

--- packages/gatsby-theme-timeline/gatsby-config.js
+++ packages/gatsby-theme-timeline/gatsby-config.js
@@ -1,42 +1,45 @@
 const withDefaults = require(`./utils/default-options`)
 module.exports = (themeOptions) => {
   const options = withDefaults(themeOptions)
-
+  let plugins = []
+  if (options.shouldTransformJson) {
+    plugins.push({
+      resolve: `gatsby-transformer-json`,
+      options: options.jsonTransformerOptions,
+    })
+  }
+  plugins = plugins.concat([
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: options.dataPath,
+        name: options.dataPath,
+      },
+    },
+    {
+      resolve: `gatsby-theme-blog-core`,
+      options: {
+        imageMaxWidth: options.imageMaxWidth,
+        ...themeOptions,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-theme-ui`,
+      options: {
+        preset: options.preset, // Allow a user to use only local shadowing with no preset
+        prismPreset: options.prismPreset,
+      },
+    },
+    {
+      resolve: `gatsby-theme-i18n`,
+      options: options.i18nConfig,
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-twitter`,
+    `gatsby-plugin-reddit`,
+    `gatsby-plugin-emotion`,
+  ])
   return {
-    plugins: [
-      options.shouldTransformJson && {
-        resolve: `gatsby-transformer-json`,
-        options: options.jsonTransformerOptions,
-      },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: options.dataPath,
-          name: options.dataPath,
-        },
-      },
-      {
-        resolve: `gatsby-theme-blog-core`,
-        options: {
-          imageMaxWidth: options.imageMaxWidth,
-          ...themeOptions,
-        },
-      },
-      {
-        resolve: `gatsby-plugin-theme-ui`,
-        options: {
-          preset: options.preset, // Allow a user to use only local shadowing with no preset
-          prismPreset: options.prismPreset,
-        },
-      },
-      {
-        resolve: `gatsby-theme-i18n`,
-        options: options.i18nConfig,
-      },
-      `gatsby-plugin-react-helmet`,
-      `gatsby-plugin-twitter`,
-      `gatsby-plugin-reddit`,
-      `gatsby-plugin-emotion`,
-    ],
+    plugins,
   }
 }
