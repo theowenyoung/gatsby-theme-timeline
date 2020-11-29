e1dbad5748e5c41bb8b458938169de3ec7614698
Owen Young <theowenyoung@gmail.com>
2020-11-29 08:57:11 +0800

    style: items title margin bottom

--- packages/gatsby-theme-timeline/src/components/items-title.js
+++ packages/gatsby-theme-timeline/src/components/items-title.js
@@ -8,43 +8,29 @@ import { LocalizedLink as Link } from "gatsby-theme-i18n"
 
 const ItemsTitle = ({ pageType, tag, basePath, currentPage }) => {
   if (pageType === `tag`) {
-    if (currentPage > 1) {
-      return (
-        <Styled.h4 sx={{ fontWeight: `normal` }}>
-          <LinkUI sx={{ color: `text` }} as={Link} to={withPrefix(basePath)}>
-            All posts
-          </LinkUI>
-          <span sx={{ color: `textMuted` }}> / </span>
-          <span sx={{ color: `textMuted` }}>{tag}</span>
-          <span sx={{ color: `textMuted` }}> / </span>
+    return (
+      <Styled.h4 sx={{ fontWeight: `normal`, mb: `1.5rem` }}>
+        <LinkUI sx={{ color: `text` }} as={Link} to={withPrefix(basePath)}>
+          All posts
+        </LinkUI>
+        <span sx={{ color: `textMuted` }}> / </span>
+        <span sx={{ color: `textMuted` }}>{tag}</span>
+        {currentPage > 1 && <span sx={{ color: `textMuted` }}> / </span>}
+        {currentPage > 1 && (
           <span sx={{ color: `textMuted` }}>page {currentPage}</span>
-        </Styled.h4>
-      )
-    } else {
-      return (
-        <Styled.h4 sx={{ fontWeight: `normal` }}>
-          <LinkUI sx={{ color: `text` }} as={Link} to={withPrefix(basePath)}>
-            All posts
-          </LinkUI>
-          <span sx={{ color: `textMuted` }}> / </span>
-          <span sx={{ color: `textMuted` }}>{tag}</span>
-        </Styled.h4>
-      )
-    }
+        )}
+      </Styled.h4>
+    )
   } else {
-    if (currentPage > 1) {
-      return (
-        <Styled.h4 sx={{ fontWeight: `normal` }}>
-          <LinkUI sx={{ color: `text` }} as={Link} to={withPrefix(basePath)}>
-            Latest
-          </LinkUI>
-          <span sx={{ color: `textMuted` }}> / </span>
+    return (
+      <Styled.h4 sx={{ fontWeight: `normal`, mb: `1.5rem` }}>
+        Latest
+        {currentPage > 1 && <span sx={{ color: `textMuted` }}> / </span>}
+        {currentPage > 1 && (
           <span sx={{ color: `textMuted` }}>page {currentPage}</span>
-        </Styled.h4>
-      )
-    } else {
-      return <Styled.h4 sx={{ fontWeight: `normal` }}>Latest</Styled.h4>
-    }
+        )}
+      </Styled.h4>
+    )
   }
 }
 
