a8fe108a5779ad8ac7765a516459a1006ed6e75e
Owen Young <theowenyoung@gmail.com>
2020-12-30 15:54:35 +0800

    feat: update react helmet

--- packages/gatsby-theme-timeline/src/components/items-seo.js
+++ packages/gatsby-theme-timeline/src/components/items-seo.js
@@ -1,11 +1,20 @@
 import React from "react"
 import SEO from "./seo"
 
-export default ({ pageContext, location }) => {
+export default ({ pageContext, location, siteMetadata }) => {
   const { pageType, tag } = pageContext
   let title = `Home`
+  let description = ``
   if (pageType === `tag`) {
     title = tag
+    description = `See all posts about ${tag} at ${siteMetadata.title}`
   }
-  return <SEO title={title} pageType={pageType} location={location} />
+  return (
+    <SEO
+      title={title}
+      description={description}
+      pageType={pageType}
+      location={location}
+    />
+  )
 }
